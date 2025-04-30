4f473c40c79ac4f5aa03fb2c8e934356af0d71c2
Resurgent99 <sstepantsevich@gmail.com>
2025-04-30 18:37:26 +0300

    Знакомство с контроллерами

---
+++ src/main/java/org/mvc/config/MySpringMvcDispatcherServletInit.java
@@ -0,0 +1,23 @@
+package org.mvc.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInit extends
+    AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?> [] getRootConfigClasses() {
+    return null;
+  }
+
+  @Override
+  protected Class<?>  [] getServletConfigClasses() {
+    return new Class[] { SpringConfiguration.class };
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {"/"};
+  }
+}
